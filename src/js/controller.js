4b2c18115188e55e2ca30788516e5354b735451a
danielemazzamuto <mazza.92@hotmail.it>
2023-04-29 15:07:23 +0200

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -127,6 +127,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('welcome to the application');
+};
+
 //Publisher-Subscriber Pattern
 //Calling a function from recipe.View (publisher) and passing another function inside
 const init = function () {
@@ -137,6 +141,6 @@ const init = function () {
   paginationView.addHandlerClick(controlPagination);
   recipeView.addHandlerUpdateServings(controlServings);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome');
+  newFeature();
 };
 init();
