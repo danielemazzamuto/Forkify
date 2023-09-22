a77e824fea202fe4b7d4f65fe74735c7d9116c4e
Daniele Mazzamuto <37002343+danielemazzamuto@users.noreply.github.com>
2023-09-22 20:27:40 +0200

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -2,5 +2,5 @@ export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const API_KEY = '';
+export const API_KEY = 'a6f1dcf9-10d4-4f3b-907c-90b0a3727354';
 export const MODAL_CLOSE_SEC = 2.5;
